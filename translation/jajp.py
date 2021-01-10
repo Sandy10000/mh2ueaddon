836c321f433530c56dc4e41da945148011012b9d
Sandy <48373918+Sandy10000@users.noreply.github.com>
2021-01-10 20:32:43 +0900

    Add files via upload

---
+++ translation/jajp.py
@@ -0,0 +1,21 @@
+# <pep8-80 compliant>
+
+dic = {
+    ("*", "Convert characters created with makehuman for Unreal Engine"):
+        "メイクヒューマンで作成したキャラクタをアンリアルエンジン用に変換する",
+    ("*", "Delete all objects"): "全オブジェクト削除",
+    ("*", "Set the unit scale to 0.01"): "単位の倍率を0.01に設定",
+    ("*", "Set Blender unit equals of makehuman add-on to Centimeter"):
+        "メイクヒューマンアドオンのブレンダーユニットをセンチメートルに設定",
+    ("*", "Press the Import human button on makehuman add-on"):
+        "メイクヒューマンアドオンのインポートヒューマンボタンを押す",
+    ("*", "Changed the object name of Armature to \"Armature\""):
+        "アーマチュアオブジェクトの名前を「Armature」に変更",
+    ("*", "Change bone vector for Unreal Engine"):
+        "ボーンの方向をアンリアルエンジン用に変更",
+    ("*", "Export in FBX format"): "FBX形式でエクスポート",
+    ("*", "Folder"): "フォルダ",
+    ("*", "File name"): "",
+    ("*", "Limit to selected objects"): "選択したオブジェクトに制限",
+    ("*", "run"): "実行",
+}
